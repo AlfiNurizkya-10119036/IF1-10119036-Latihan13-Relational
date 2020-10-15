2d2d356eb3aadddab175b8fe30cce23b8c823d60
ALFI22-01 <72850530+ALFI22-01@users.noreply.github.com>
2020-10-15 13:50:09 +0700

    Project 13

---
+++ src/PBO10119036Latihan13Relational.java
@@ -0,0 +1,23 @@
+
+/**
+ *
+ * @author Alfi Nurizkya
+ * NAMA     : ALFI NURIZKYA
+ * KELAS    : PBO
+ * NIM      : 10119036
+ * DESKRIPSI PROGRAM : PERINTAH MEMASUKKAN DATA FORMATING
+ */
+public class PBO10119036Latihan13Relational {
+    
+    public static void main(String[] args) {
+       int bil1 = 10;
+       int bil2 = 20;
+       System.out.println("a == b = " + (bil1 == bil2));
+       System.out.println("a != b = " + (bil1 != bil2));
+       System.out.println("a > b = " + (bil1 > bil2));
+       System.out.println("a < b = " + (bil1 < bil2));
+       System.out.println("a >= b = " + (bil1 >= bil2));
+       System.out.println("a <= b = " + (bil1 <= bil2));
+    }
+    
+}
